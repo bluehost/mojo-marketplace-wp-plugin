a6c82605a1f7344c94cecd2cf1caa068e5848581
Evan Mullins <evanrm@gmail.com>
2022-07-26 17:01:34 -0400

    fresh build files

--- build/marketplace.asset.php
+++ build/marketplace.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-dom-ready', 'wp-element'), 'version' => 'e884d5708829a64e7a98');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-dom-ready', 'wp-element'), 'version' => 'c34f6652075ba2c5cffd');
