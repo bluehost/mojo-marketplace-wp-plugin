e346a06eda58837cc93ea4e6042155909e784931
Evan Mullins <evanrm@gmail.com>
2023-04-26 11:47:10 -0400

    add bn code filter from ecom module

--- bootstrap.php
+++ bootstrap.php
@@ -102,3 +102,36 @@ $mojo_plugin_updater->setDataMap(
 		'tested'        => 'tested.wp',
 	)
 );
+
+/**
+ * Filter to add applicable BN code to paypal requests
+ * 
+ * https://github.com/newfold-labs/wp-module-ecommerce/blob/trunk/bootstrap.php#L31-L57
+ */
+if ( function_exists( 'add_filter' ) ) {
+
+	add_filter(
+		'http_request_args',
+		function ( $parsed_args, $url ) {
+
+			// Bail early if the request is not to PayPal's v2 checkout API
+			if ( false === stripos( wp_parse_url( $url, PHP_URL_HOST ), 'paypal.com' ) ) {
+				return $parsed_args;
+			}
+
+			// Check for an existing bn_code
+			$bn_code = isset( $parsed_args['headers']['PayPal-Partner-Attribution-Id'] ) ? $parsed_args['headers']['PayPal-Partner-Attribution-Id'] : null;
+
+			// Ensure we only set when blank, or when using one of our stale codes
+			if ( is_null( $bn_code ) || false !== stripos( $bn_code, 'yith' ) || false !== stripos( $bn_code, 'newfold' ) ) {
+				// The correct code is case-sensitive. YITH brand is uppercase, but the code is not.
+				$parsed_args['headers']['PayPal-Partner-Attribution-Id'] = 'Yith_PCP';
+			}
+
+			return $parsed_args;
+		},
+		10,
+		2
+	);
+
+}
