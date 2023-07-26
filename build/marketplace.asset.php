de4edfc4e7572babb42aa443fbee75475ae5be39
Evan Mullins <evanrm@gmail.com>
2023-07-26 12:55:57 -0500

    version bump and package updates

--- build/marketplace.asset.php
+++ build/marketplace.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-dom-ready', 'wp-element'), 'version' => 'a43ca9d875471d4571cb');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-dom-ready', 'wp-element'), 'version' => 'a315203eef7f4fc60545');
