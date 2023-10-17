9687e45c60cd2bf499b2b600792dc3bd79b8bf92
Evan Mullins <evanrm@gmail.com>
2023-10-17 12:55:46 -0400

    plugin version update to 1.7.9

--- build/marketplace.asset.php
+++ build/marketplace.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-dom-ready', 'wp-element'), 'version' => 'adc59faf7795e99611f3');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-dom-ready', 'wp-element'), 'version' => '12b0140fa860636b3b5e');
